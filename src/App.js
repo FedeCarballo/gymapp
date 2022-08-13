b076b180a184c92c1f802ed1eb4d98a9135d3578
FedeCarballo <carballo523@gmail.com>
2022-08-13 11:48:06 -0300

    estructura basica

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import {Box} from '@mui/material';
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Pages/Home'
+import ExerciseDetail from './Pages/ExerciseDetail'
+import Footer from './Components/Footer/Footer';
+import {IntlProvider} from 'react-intl';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <IntlProvider messages={{}}>
+      <Box width="400px" sx={{width:{xl:'1488px'}}} m="auto">
+            <Navbar />
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/Exercise/:id" element={<ExerciseDetail /> }/>
+        </Routes>
+            <Footer />
+      </Box>
+    </IntlProvider>
   );
 }
 
